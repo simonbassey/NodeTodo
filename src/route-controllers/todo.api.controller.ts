036c3c10a85e734ca56abb60151b2a6352a2d1ee
simonbassey <simon.dev.bassey@gmail.com>
2019-10-07 16:50:01 +0100

    updated todo api route

--- src/route-controllers/todo.api.controller.ts
+++ src/route-controllers/todo.api.controller.ts
@@ -1,9 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import TodoRepository from "./../core/data/todo.repository";
+import {TodoMongooseRepository} from "./../core/data/todo.mongoose.repository";
 import TodoItem from "./../core/models/todos";
+import { TodoDocument } from "../core/models/domain/todo.model";
 
 const router = express.Router();
-const todoRepository = new TodoRepository();
+// const todoRepository = new TodoRepository();
+const todoRepository = new TodoMongooseRepository();
 router.get("/", (req, res) => {
     const welcomeMsg = `Hello ${req.ip} .. Welcome to classic Todo API`;
     res.write(welcomeMsg);
@@ -23,12 +26,12 @@ router.get("/api/todos", (req, res) => {
 
 router.post("/api/todos/create", async (req, res) => {
     try {
-        const payload: any = { title: req.body["title"], description: req.body["description"]};
+        const payload: any = { title: req.body["title"], description: req.body["description"], userId: req.body["userId"]};
         const validationErrors = validateReqPayload(payload);
         if (validationErrors.length > 0) {
             return res.status(400).json(validationErrors);
         }
-        if (await todoExist(payload.title)) {
+        if (await todoExist(payload.userId, payload.title)) {
             return res.status(400).json(`A similar activity with the same title '${payload.title}' already exist`);
         }
         const result = await todoRepository.addTodo(payload);
@@ -40,26 +43,47 @@ router.post("/api/todos/create", async (req, res) => {
 });
 
 router.get("/api/todos/:id", async (req, res) => {
-    const id = req.route.id;
-    const todo = await todoRepository.getTodo(id);
-    return res.status(200).json(todo);
+    try {
+        const id = req.params.id;
+        const todo = await todoRepository.getTodo(id);
+        return res.status(200).json(todo);
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 router.put("/api/todos/:id/update", async (req, res) => {
-    const id = req.route.id;
-    const payload = {title: req.body["title"], description: req.body["description"]} as TodoItem;
-    const validationErrors = validateReqPayload(payload);
-    if (validationErrors.length > 0) {
-        return res.status(400).json(validationErrors);
+    try {
+        const id = req.params.id;
+        const payload = {title: req.body["title"], description: req.body["description"], completed: req.body["completed"], userId: req.body["userId"]} as TodoDocument;
+        const validationErrors = validateReqPayload(payload);
+        if (validationErrors.length > 0) {
+            return res.status(400).json(validationErrors);
+        }
+        const updateResult = await todoRepository.updateTodo(id, payload);
+        if (!updateResult){
+            return res.status(400).send({status: false, message: "Failed to update todo item"});
+        }
+        return res.status(200).send({status: true, message: "successfully updated item"});
     }
-    const updateResult = await todoRepository.updateTodo(id, payload);
-    if (updateResult.status){
-        return res.status(400).json(`${updateResult.message}`);
+    catch (error) {
+        return res.status(500).send(error);
     }
-    return res.status(204).send();
 });
 
-function validateReqPayload(payload: any): any[] {
+router.delete("/api/todos/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const deleted = await todoRepository.deleteTodo(_id);
+        return res.status(200).send({status: deleted ? "success" : "failed", message: deleted ? "Requested Todo has been removed from db" : "Failed to remove requested todo from the database"});
+    }
+    catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+function validateReqPayload(payload: TodoDocument): any[] {
     const errors = [];
     if (!payload.title || payload.title.trim().length < 3){
         errors.push({param: "title", error: "Invalid title. must be 3 characters or more"});
@@ -67,12 +91,18 @@ function validateReqPayload(payload: any): any[] {
     if (!payload.description || payload.description.trim().length < 1) {
         errors.push({param: "description", error: "Invalid. Please provide a valid description for this activity"});
     }
+    if (!payload.userId || payload.userId.trim().length < 1) {
+        errors.push({param: "userId", error: "You must be signed in as a user to create Todo items"});
+    }
+
     return errors;
 }
 
-async function todoExist(title: string): Promise<boolean> {
+async function todoExist(userId: string, title: string): Promise<boolean> {
     title = title ? title.toLowerCase().trim() : "";
-    return (await todoRepository.getTodoByTitle(title)) != null;
+    const userTodos = await todoRepository.filterTodos({userId});
+    const todo = userTodos.length < 1 ? null : userTodos.find((td) => td.title.toLowerCase() === title.toLowerCase());
+    return todo && todo.title ? true : false;
 }
 
 export default router;
