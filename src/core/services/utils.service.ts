4964463ce272a87e7c14a09a99cf39a4091a0259
simonbassey <simon.dev.bassey@gmail.com>
2019-10-15 20:31:01 +0100

    Added Bcrypt module for a more time-sensitive hash functions

--- src/core/services/utils.service.ts
+++ src/core/services/utils.service.ts
@@ -1,4 +1,5 @@
 import crypto from "crypto";
+import bcrypt from "bcrypt";
 export class Util {
     public static Hash(algo: string, plainText: string, salt: string = ""): string {
         salt = salt ? salt : crypto.randomBytes(16).toString("base64");
@@ -12,5 +13,28 @@ export class Util {
             throw new Error("Invalid hash algorithm requested");
         }
         return crypto.createHmac(algo, salt).update(plainText).digest("base64");
+    }
+
+    public static BcryptHash(plainText: string, rounds: number = 10): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            bcrypt.hash(plainText, rounds, (err, encryptedStr) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(encryptedStr);
+            });
+        });
+    }
+
+    public static BcryptCompare(plainText: string, encryptedhash: string): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            bcrypt.compare(plainText, encryptedhash, (err, result) => {
+                if (err) {
+                    reject (err);
+                }
+                resolve(result);
+            });
+        });
     }
 }
